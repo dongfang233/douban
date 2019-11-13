eb6b10471a01ad7663e221ce9f4d7ce6086c41f8
dongfang233 <57612796+dongfang233@users.noreply.github.com>
2019-11-13 09:51:40 +0800

    award

---
+++ liuhaoranjava/DoubanDAO/src/douban/dao/Award.java
@@ -0,0 +1,18 @@
+package douban.dao;
+
+public class Award {
+	String awardid;
+	String prize;
+	public String getAwardid() {
+		return awardid;
+	}
+	public void setAwardid(String awardid) {
+		this.awardid = awardid;
+	}
+	public String getPrize() {
+		return prize;
+	}
+	public void setPrize(String prize) {
+		this.prize = prize;
+	}
+}
