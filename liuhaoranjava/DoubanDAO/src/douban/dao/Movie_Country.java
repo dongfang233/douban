0c6d7476c7090c9db983927a597e577549b30cbc
dongfang233 <57612796+dongfang233@users.noreply.github.com>
2019-11-13 16:51:30 +0800

    Create Movie_Country.java

---
+++ liuhaoranjava/DoubanDAO/src/douban/dao/Movie_Country.java
@@ -0,0 +1,18 @@
+package douban.dao;
+
+public class Movie_Country {
+	String movieid;
+	String countryid;
+	public String getMovieid() {
+		return movieid;
+	}
+	public void setMovieid(String movieid) {
+		this.movieid = movieid;
+	}
+	public String getCountryid() {
+		return countryid;
+	}
+	public void setCountryid(String countryid) {
+		this.countryid = countryid;
+	}
+}
