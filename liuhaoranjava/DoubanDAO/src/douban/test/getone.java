fd33a43f1d4c97366d48d4fa393b9afe3f061ff6
Liuhaoran812 <57614493+Liuhaoran812@users.noreply.github.com>
2019-11-15 12:32:16 +0800

    Update getone.java

--- liuhaoranjava/DoubanDAO/src/douban/test/getone.java
+++ liuhaoranjava/DoubanDAO/src/douban/test/getone.java
@@ -1,4 +1,8 @@
 package douban.test;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import douban.dao.*;
 
 public class getone {
@@ -6,10 +10,14 @@ public class getone {
 	public static void main(String[] args) {
 		String userid=null;
 		String movieid=null;
+		Format f=new SimpleDateFormat("yyyy-MM-dd");
+		Calendar c=Calendar.getInstance();
 		User user=DAOFactory.getUserDAO().getUser(userid);
 		System.out.println("userid="+user.getUserid()+" username="+user.getUsername()+" password="+user.getPassword()+" phonenum="+user.getPhonenum()+" email="+user.getEmail()+" usertype="+user.getUsertype());
 		Movie movie=DAOFactory.getMovieDAO().getMovie(movieid);
-		System.out.println("movieid="+movie.getMovieid()+" moviename="+movie.getMoviename()+" language="+movie.getLanguage()+" date="+movie.getDate()+" time="+movie.getTime()+" aliasname="+movie.getAliasname()+" moviepicture="+movie.getMoviepicture());
+		c.setTime(movie.getDate());
+		c.add(Calendar.DAY_OF_MONTH, 2);
+		System.out.println("movieid="+movie.getMovieid()+" moviename="+movie.getMoviename()+" language="+movie.getLanguage()+" date="+f.format(c.getTime())+" time="+movie.getTime()+" aliasname="+movie.getAliasname()+" moviepicture="+movie.getMoviepicture());
 	}
 
 }
