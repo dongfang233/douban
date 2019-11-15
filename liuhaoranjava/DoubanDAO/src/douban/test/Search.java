d687aa9117e9e5922ae72b64337e83ee7c9ba855
Liuhaoran812 <57614493+Liuhaoran812@users.noreply.github.com>
2019-11-15 11:08:49 +0800

    Update Search.java

--- liuhaoranjava/DoubanDAO/src/douban/test/Search.java
+++ liuhaoranjava/DoubanDAO/src/douban/test/Search.java
@@ -3,23 +3,32 @@ import java.util.*;
 import douban.dao.*;
 public class Search {
 
-	public static void main(String[] args) {
-		//Search
+	static void Print_User(String sql) {
 		List<User> users=new ArrayList<User>();
-		users=DAOFactory.getUserDAO().getUserByC("");
+		users=DAOFactory.getUserDAO().getUserByC(sql);
 		Iterator<User> it=users.iterator();
 		while(it.hasNext()) {
 			User user=it.next();
 			System.out.println("userid="+user.getUserid()+" username="+user.getUsername()+" password="+user.getPassword()+" phonenum="+user.getPhonenum()+" email="+user.getEmail()+" usertype="+user.getUsertype());
 		}
-		
+	}
+	static void Print_Movie(String sql) {
 		List<Movie> movies=new ArrayList<Movie>();
 		movies=DAOFactory.getMovieDAO().getMovieByC("");
-		Iterator<Movie> it1=movies.iterator();
+		Iterator<Movie> it=movies.iterator();
 		while(it.hasNext()) {
-			Movie movie=it1.next();
+			Movie movie=it.next();
 			System.out.println("movieid="+movie.getMovieid()+" moviename="+movie.getMoviename()+" language="+movie.getLanguage()+" date="+movie.getDate()+" time="+movie.getTime()+" aliasname="+movie.getAliasname()+" moviepicture="+movie.getMoviepicture());
 		}
+	}
+	public static void main(String[] args) {
+		//Search
+		Print_User("");
+		
+		Print_Movie("");
+		
+		//search label="fantasy" movie information
+		Print_Movie("movieid=any(select movieid from Movie_Label where labelid=any(select labelid from Label where labelname='fantasy'))");
 	}
 
 }
