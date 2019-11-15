1d8a6f0590dece1bbf9a9eb4733450ba7cff6a58
dongfang233 <57612796+dongfang233@users.noreply.github.com>
2019-11-15 11:35:25 +0800

    Update getone.java

--- liuhaoranjava/DoubanDAO/src/douban/test/getone.java
+++ liuhaoranjava/DoubanDAO/src/douban/test/getone.java
@@ -6,8 +6,10 @@ public class getone {
 	public static void main(String[] args) {
 		String userid=null;
 		String movieid=null;
-		DAOFactory.getUserDAO().getUser(userid);
-		DAOFactory.getMovieDAO().getMovie(movieid);
+		User user=DAOFactory.getUserDAO().getUser(userid);
+		System.out.println("userid="+user.getUserid()+" username="+user.getUsername()+" password="+user.getPassword()+" phonenum="+user.getPhonenum()+" email="+user.getEmail()+" usertype="+user.getUsertype());
+		Movie movie=DAOFactory.getMovieDAO().getMovie(movieid);
+		System.out.println("movieid="+movie.getMovieid()+" moviename="+movie.getMoviename()+" language="+movie.getLanguage()+" date="+movie.getDate()+" time="+movie.getTime()+" aliasname="+movie.getAliasname()+" moviepicture="+movie.getMoviepicture());
 	}
 
 }
